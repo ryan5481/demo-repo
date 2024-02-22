ae827c81af5c9edfb6e94ea9831308459289885d
ryan5481 <ryan5481@me.com>
2024-02-23 01:00:20 +0800

    modified appjs in MAIN branch

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function app() {
   return (
-    <div>This is the new JSX content</div>
+    <>
+    <div>This is the MODIFIED CONTENT in main branch.</div>
+    </>
   )
 }
 
